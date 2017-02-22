02e380e8b0ec3373399ca653ddb7cd6a5207dcd3
nicerobot <nicerobot@users.noreply.github.com>
2017-02-21 20:09:16 -0800

    -return

--- client/run.go
+++ client/run.go
@@ -12,7 +12,7 @@ type MainFunc func(servicer.Settings) error
 type Runner func(MainFunc) cli.ActionFunc
 
 //
-func Main(main MainFunc, name, usage string) cli.ActionFunc {
+func Main(main MainFunc, name, usage string) {
 	config := func(app *cli.App) cli.ActionFunc {
 		app.Name = name
 		app.Usage = usage
